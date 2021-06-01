b3cf1b28cd6dfbff1265a6039b65469cca3dda68
Joshua <joycejosh7@gmail.com>
2021-06-01 15:36:22 -0400

    added fetch

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
 export const getServices = () => {
     return dispatchEvent => {
         dispatchEvent({ type: "LOADING"})
+        fetch("http://localhost:3001/services")
+        .then(resp => resp.json())
+        .then(services => dispatchEvent({ type: "SET_SERVICES", services}))
     }
 }
