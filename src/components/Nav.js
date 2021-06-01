52b490a5b93e4e9a31222b1fbf187292ce0f0caf
Joshua <joycejosh7@gmail.com>
2021-06-01 13:29:43 -0400

    added home page link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Nav extends Component {
     render() {
         return (
             <div>
-                Nav
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                </ul>
             </div>
         )
     }
