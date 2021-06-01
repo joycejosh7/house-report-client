784a4d30629e836ad650cf4346766411a989e226
Joshua <joycejosh7@gmail.com>
2021-06-01 12:44:05 -0400

    created default state of reducer

---
+++ src/reducers/servicesReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    services: [],
+    loading: true
+}
+
+const servicesReducer = (state, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
