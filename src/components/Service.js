87a245fb71b01a0fb7b5e6e9655edfc119ef876a
Joshua <joycejosh7@gmail.com>
2021-06-01 15:37:34 -0400

    added service details

--- src/components/Service.js
+++ src/components/Service.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react'
 
 class Service extends Component {
     render() {
+        const { title, contractor, company, description, date } = this.props
         return (
             <div>
-                Service
+                <h3>{title}</h3>
+                <p>{contractor}</p>
+                <p>{company}</p>
+                <p>{description}</p>
+                <p>{date}</p>
             </div>
         )
     }
