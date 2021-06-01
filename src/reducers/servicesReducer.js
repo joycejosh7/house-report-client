2ba3b378c5d76ca7c21d1a5143514929adaaf211
Joshua <joycejosh7@gmail.com>
2021-06-01 15:37:55 -0400

    added case loading and set-services

--- src/reducers/servicesReducer.js
+++ src/reducers/servicesReducer.js
@@ -5,6 +5,17 @@ const initialState = {
 
 const servicesReducer = (state= initialState, action) => {
     switch(action.type) {
+        case "LOADING":
+            return {
+                ...state,
+                loading: true
+            }
+        case "SET_SERVICES":
+            return {
+                ...state,
+                loading: false,
+                services: action.services
+            }
         default:
             return state
     }
