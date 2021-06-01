debba6a27f51b6a5c98aede2cafea4efd84bb411
Joshua <joycejosh7@gmail.com>
2021-06-01 15:37:09 -0400

    added connect

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Service from './Service';
 
 class Index extends Component {
     render() {
+        const services = this.props.services.map( (service, i) => <Service key={i} title={service.title} contractor={service.contractor} company={service.company} description={service.description} date={service.date}/>)
+
         return (
             <div>
-               List Page 
+               { services } 
             </div>
         )
     }
 }
 
-export default Index
+const mapStateToProps = state => {
+    return {
+        services: state.services
+    }
+}
+
+export default connect(mapStateToProps)(Index)
