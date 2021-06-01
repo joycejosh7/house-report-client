3164ea76e6cf31b97527fedca2af072a50e916c1
Joshua <joycejosh7@gmail.com>
2021-06-01 13:20:36 -0400

    created home page

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Home Services</h1>
+
+            </div>
+        )
+    }
+}
+
+export default Home
