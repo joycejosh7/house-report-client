0cfd573488e52e822240bd8d1577c49111448e60
Joshua <joycejosh7@gmail.com>
2021-06-01 15:52:24 -0400

    started form creation

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,28 @@ class Form extends Component {
     render() {
         return (
             <div>
-                Form
+                <h2>Add Service</h2>
+                <form>
+                    <div>
+                        <label htmlFor="title">Service</label>
+                        <input type="text" id="title" />
+                    </div>
+                    <div>
+                        <label htmlFor="contractor">Contractor Name</label>
+                        <input type="text" id="contractor" />
+                    </div>
+                    <div>
+                        <label htmlFor="company">Company Name</label>
+                        <input type="text" id="company" />
+                    </div>
+                        <label htmlFor="description">Description of Service</label>
+                        <textarea id="description"></textarea>
+                    <div>
+                        <label htmlFor="date">Date of Service</label>
+                        <input type="text" id="date" />
+                    </div>
+
+                </form>
             </div>
         )
     }
