28b8645c4e608554bf5e502b502a92a4caaeca7b
Joshua <joycejosh7@gmail.com>
2021-06-01 14:57:06 -0400

    added about page write up

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,10 @@ class About extends Component {
     render() {
         return (
             <div>
-                About page
+                <h2>About page</h2>
+                <p>Finally there is a place to keep track of all those home improvements and maintenace items.</p>
+                <p>Home Service is the application that will keep all your home information organized.</p>
+                
             </div>
         )
     }
