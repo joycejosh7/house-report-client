b7ae0730c3ced1ecbeeac565985176c8c7730ce8
Joshua <joycejosh7@gmail.com>
2021-06-02 10:08:56 -0400

    completed form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,34 +1,62 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { addService } from '../actions'; 
 
 class Form extends Component {
+    state = {
+        title: "",
+        contractor: "",
+        company: "",
+        description: "",
+        date: ""
+    }
+
+    handleChange = e => {
+        this.setState({
+          [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.addService(this.state, this.props.history)
+    }
+
     render() {
         return (
             <div>
                 <h2>Add Service</h2>
-                <form>
+                <form onSubmit={ this.handleSubmit }>
                     <div>
-                        <label htmlFor="title">Service</label>
-                        <input type="text" id="title" />
+                        <label htmlFor="title">Service </label>
+                        <input type="text" id="title" name="title" value={ this.state.title } onChange={ this.handleChange } />
                     </div>
+                    <br/>
                     <div>
-                        <label htmlFor="contractor">Contractor Name</label>
-                        <input type="text" id="contractor" />
+                        <label htmlFor="contractor">Contractor Name </label>
+                        <input type="text" id="contractor" name="contractor" value={ this.state.contractor } onChange={ this.handleChange } />
                     </div>
+                    <br/>
                     <div>
-                        <label htmlFor="company">Company Name</label>
-                        <input type="text" id="company" />
+                        <label htmlFor="company">Company Name </label>
+                        <input type="text" id="company" name="company" value={ this.state.company } onChange={ this.handleChange } />
                     </div>
-                        <label htmlFor="description">Description of Service</label>
-                        <textarea id="description"></textarea>
+                    <br/>
                     <div>
-                        <label htmlFor="date">Date of Service</label>
-                        <input type="text" id="date" />
+                        <label htmlFor="description">Description of Service </label>
+                        <textarea id="description" name="description" value={ this.state.description } onChange={ this.handleChange } ></textarea>
                     </div>
-
+                    <br/>
+                    <div>
+                        <label htmlFor="date">Date of Service </label>
+                        <input type="text" id="date" name="date" value={ this.state.date } onChange={ this.handleChange } />
+                    </div>
+                    <br/>
+                    <input type="submit" value="Add Service" />
                 </form>
             </div>
         )
     }
 }
 
-export default Form
+export default connect(null, { addService })(Form)
