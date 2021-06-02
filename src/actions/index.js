fcd6fc150aa898887c0a41826877b8252edbe63b
Joshua <joycejosh7@gmail.com>
2021-06-02 10:09:18 -0400

    added POST fetch

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,21 @@ export const getServices = () => {
         .then(services => dispatchEvent({ type: "SET_SERVICES", services}))
     }
 }
+
+export const addService = (service, history) => {
+    return dispatch => {
+      fetch('http://localhost:3001/services', {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ service })
+      })
+        .then(resp => resp.json())
+        .then(service => {
+          dispatch({ type: "ADD_SERVICE", service })
+          history.push("/services")
+        })
+    }
+  }
