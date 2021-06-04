88821bd515ba8fb8cd08da757a3046c564cae7ce
Joshua <joycejosh7@gmail.com>
2021-06-04 12:35:36 -0400

    applied styling and lables

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
+// import { deleteService } from '../actions'
 
 class Service extends Component {
     render() {
         const { title, contractor, company, description, date } = this.props
+
+        // const handleDelete = () => {
+        //     debugger
+        //     this.props.deleteService(this.props.service.id)
+        //     this.props.history.push("/services")
+        //   }
+
+        const serviceStyle = {
+            backgroundColor:"grey",
+        }
+
         return (
-            <div>
+
+            <div style={serviceStyle}>
                 <h3>{title}</h3>
-                <p>{contractor}</p>
-                <p>{company}</p>
-                <p>{description}</p>
+                <p>Serviced by:  {contractor}</p>
+                <p>From: {company}</p>
+                <p>Services Rendered: {description}</p>
                 <p>{date}</p>
+                {/* <button onClick={handleDelete}>Delete</button> */}
             </div>
         )
     }
